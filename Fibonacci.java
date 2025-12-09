9bfa97597aefa0d84a2ac9c3486f11b99cbb2ca6
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-09 10:18:48 -0500

    Create Fibonacci class with main method
    Initial implementation of Fibonacci class with a method stub.

---
+++ Fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci{
+  //F(0) = 0,   F(1) = 1,  and  F(n) = F(n-1) + F(n-2) for any n >=2
+  public static void f(){
+    if(
+      
+  }
+
+  public static void main(String[] args){
+    Fibonacci.f();
+  }
+}
