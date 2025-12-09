b1c9c6ba0f894e81626972da4c487912554d1d3a
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-09 10:53:29 -0500

    finalized code

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,19 +1,21 @@
 public class Fibonacci {
   public static int f(int n){
         if(n==0){
-            System.out.print(n+ " ");
+            // System.out.print(n+ " ");
             return 0;
         } else if(n==1){
-            System.out.print(n+ " ");
+            // System.out.print(n+ " ");
             return 1;
         } else{
-            System.out.print(n+ " ");
+            // System.out.print(n+ " ");
             return f(n-1) + f(n-2);
         }
     }
     //F(0) = 0,   F(1) = 1,  and  F(n) = F(n-1) + F(n-2) for any n >=2
 
   public static void main(String args[]) {
-    Fibonacci.f(20);
+    for(int i=0; i<20; i++){
+        System.out.println(Fibonacci.f(i));
+    }
   }
 }
