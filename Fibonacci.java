f35b645feb3c699cb68ad514bd19ae60911bbecd
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-09 10:49:24 -0500

    fix Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,17 @@
 public class Fibonacci {
   public static int f(int n){
         if(n==0){
-          return 0;
-        } else {
-          System.out.println(n+" ");
-          return f(n-1) + f(n-1);
+            System.out.print(n+ " ");
+            return 0;
+        } else if(n==1){
+            System.out.print(n+ " ");
+            return 1;
+        } else{
+            System.out.print(n+ " ");
+            return f(n-1) + f(n-2);
         }
     }
+    //F(0) = 0,   F(1) = 1,  and  F(n) = F(n-1) + F(n-2) for any n >=2
 
   public static void main(String args[]) {
     Fibonacci.f(20);
