41a0e0ec3f43f417f719f75b78fb4f1906ca00cc
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-09 10:30:41 -0500

    Fibonacci calculation but infinite loop
    Fix Fibonacci function to correctly calculate F(n) and update main method to call it with an argument.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,14 @@
-public class Fibonacci{
-  //F(0) = 0,   F(1) = 1,  and  F(n) = F(n-1) + F(n-2) for any n >=2
+public class Fibonacci {
   public static int f(int n){
-    if(n==0){
-      return 0;
-    } else {
-      System.out.println(n+" ");
-      return f(n-1) = f(n-1);
-  }
+        if(n==0){
+          return 0;
+        } else {
+          System.out.println(n+" ");
+          return f(n-1) + f(n-1);
+        }
+    }
 
-  public static void main(String[] args){
-    Fibonacci.f();
+  public static void main(String args[]) {
+    Fibonacci.f(20);
   }
 }
