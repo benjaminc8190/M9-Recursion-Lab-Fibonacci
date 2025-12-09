bcc016d80f053292bd4790451a10a70e553b57a1
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-09 10:23:25 -0500

    Refactor Fibonacci function to take an argument

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,11 @@
 public class Fibonacci{
   //F(0) = 0,   F(1) = 1,  and  F(n) = F(n-1) + F(n-2) for any n >=2
-  public static void f(){
-    if(
-      
+  public static int f(int n){
+    if(n==0){
+      return 0;
+    } else {
+      System.out.println(n+" ");
+      return f(n-1) = f(n-1);
   }
 
   public static void main(String[] args){
